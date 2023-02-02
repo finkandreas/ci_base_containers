80c0c746e659f296ac17babb7ab9f2b961550bfa
Andreas Fink <finkandreas@web.de>
2023-02-02 09:27:01 +0100

    reworked most things, should be working fine with spack-0.19.x and spack-0.18.x

--- build_containers_and_push.sh
+++ build_containers_and_push.sh
@@ -2,28 +2,26 @@
 
 set -e
 
-for spackver in "v0.19.0" ; do
+for spackver in "v0.18.1" "v0.19.0" ; do
     for baseimg in docker.io/ubuntu:22.04 ; do
         SPACK_DOCKER_TAG=$(echo $spackver | sed -e 's/^v//')
         OS_DOCKER_TAG=$(basename "$baseimg" | sed -e 's/://')
         DOCKER_TAG=finkandreas/spack:${SPACK_DOCKER_TAG}-${OS_DOCKER_TAG}
-        BASE_RETAG_NAME=finkandreas/spack:base-${OS_DOCKER_TAG}
-        docker build -f docker/Dockerfile_spack_baseimage_${OS_DOCKER_TAG}_cpu --build-arg BASEIMG=$baseimg --build-arg SPACK_VER=$spackver -t ${DOCKER_TAG} .
-        docker tag $baseimg "$BASE_RETAG_NAME"
-        docker push "${BASE_RETAG_NAME}"
-        docker push "${DOCKER_TAG}"
+        BASE_TAG_NAME=finkandreas/spack:base-${OS_DOCKER_TAG}
+        podman build -f docker/Dockerfile_spack_baseimage_${OS_DOCKER_TAG} --format docker --build-arg BASEIMG=$baseimg --build-arg SPACK_VER=$spackver -t ${DOCKER_TAG} .
+        podman build -f docker/Dockerfile_base_helper --format docker --build-arg BASEIMG=$baseimg -t ${BASE_TAG_NAME} .
+        podman push "${BASE_TAG_NAME}"
+        podman push "${DOCKER_TAG}"
 
         # do the same for cuda base images
         for cudaver in "11.7.1" ; do
             cuda_baseimg=docker.io/nvidia/cuda:${cudaver}-devel-${OS_DOCKER_TAG}
-            cuda_rtimg=docker.io/nvidia/cuda:${cudaver}-devel-${OS_DOCKER_TAG}
-            CUDA_BASE_RETAG_NAME=finkandreas/spack:base-cuda${cudaver}-${OS_DOCKER_TAG}
+            CUDA_BASE_TAG_NAME=finkandreas/spack:base-cuda${cudaver}-${OS_DOCKER_TAG}
             CUDA_DOCKER_TAG=finkandreas/spack:${SPACK_DOCKER_TAG}-cuda${cudaver}-${OS_DOCKER_TAG}
-            docker build -f docker/Dockerfile_spack_baseimage_${OS_DOCKER_TAG}_gpu --build-arg BASEIMG=$cuda_baseimg --build-arg SPACK_VER=$spackver -t ${CUDA_DOCKER_TAG} .
-            docker pull $cuda_rtimg
-            docker tag $cuda_rtimg $CUDA_BASE_RETAG_NAME
-            docker push $CUDA_BASE_RETAG_NAME
-            docker push $CUDA_DOCKER_TAG
+            podman build -f docker/Dockerfile_spack_baseimage_${OS_DOCKER_TAG} --format docker --build-arg BASEIMG=$cuda_baseimg --build-arg SPACK_VER=$spackver -t ${CUDA_DOCKER_TAG} .
+            podman build -f docker/Dockerfile_base_helper --format docker --build-arg BASEIMG=$cuda_baseimg -t ${CUDA_BASE_TAG_NAME} .
+            podman push $CUDA_BASE_TAG_NAME
+            podman push $CUDA_DOCKER_TAG
         done
     done
 done
